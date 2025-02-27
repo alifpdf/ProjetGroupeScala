d8c83132bb5bbb6e8f82a92d3a936260c828a3fd
alifpdf <alif.rafayel@hotmail.fr>
2025-02-27 16:11:07 +0100

    Ajout main et script

---
+++ src/main/script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+                                     id SERIAL PRIMARY KEY,
+                                     name VARCHAR(100),
+    email VARCHAR(150) UNIQUE
+    );
+
+INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
